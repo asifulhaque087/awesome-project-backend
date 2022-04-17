9c668fe33027ff7ace26366d63f96f406b0917a3
asiful087 <asifulhaque087@gmail.com>
2022-04-17 08:21:52 +0600

    add comment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     PostModule,
+    CommentModule,
   ],
   // imports: [],
   // controllers: [AppController],
