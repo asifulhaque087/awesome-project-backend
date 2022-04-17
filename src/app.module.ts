9af13112b5c3c3392a2dc290ec6f2f19ea30ae67
asiful087 <asifulhaque087@gmail.com>
2022-04-17 08:14:46 +0600

    set up mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
+  // imports: [],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
